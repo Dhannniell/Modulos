ff16f59664ffcaf7917c344112e933f1f8972f1a
Daniel G. Torres <danielguevara1500@gmail.com>
2025-05-19 14:40:12 -0500

    commit

--- Modulo_4/Sesion_1/conjunto.py
+++ Modulo_4/Sesion_1/conjunto.py
@@ -17,9 +17,9 @@ def gestionar_clientes(clientes: Set[str]) -> None:
     print("Cliented despues de agregar intentar agregar a Carlos", clientes)
     
     # Uso de la funcion "remove" para eliminar un cliente (si existe el cliente, y si no -> ERROR)
-    clientes = "Ana"
-    clientes.remove("Ana")
-    print("Cliented despues de eliminar a Ana con el metodo remove", clientes)
+    cliente = "Ana"
+    clientes.remove(cliente)
+    print("Clientes despues de eliminar a Ana con el metodo remove", clientes)
     
     # Uso de la funcion discard para eliminar un elemento del Set 
     cliente2 = "Luis"
@@ -30,3 +30,19 @@ def gestionar_clientes(clientes: Set[str]) -> None:
     cliente_removido = clientes.pop()
     print(f"Cliente removido aleatoriamente: {cliente_removido}")
     print("Clientes restantes:", clientes)
+    
+    # Borrar todos los elementos del Set 
+    clientes.clear()
+    print("Clientes despues del metodo Clear: ", clientes)
+    
+def main():
+    
+    clientes_antiguos = {"Carlos", "Ana", "Luis"}
+    clientes_nuevos = {"Luis", "Maria", "Jorge"}
+    clientes_finales = obtener_clientes_unicos(clientes_antiguos, clientes_nuevos)
+    print("La lista actualizada de clientes es : ", clientes_finales)
+    
+    gestionar_clientes(clientes_finales)
+
+if __name__ == "__main__":
+    main()
