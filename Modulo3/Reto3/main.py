ffeb23930022a12f36fc5ed4114dd7df2dff3ba7
Daniel G. Torres <danielguevara1500@gmail.com>
2025-07-23 19:49:51 -0500

    commit

---
+++ Modulo3/Reto3/main.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+#Modelo 
+class Producto:
+    def __init__(self, id_producto, nombre, precio, stock):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+        pass
+
+
+#Controlador 
+
+#Vista
+
+#Main
+
